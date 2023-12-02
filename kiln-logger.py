6eb3cf37f27914fef16406912babba89d5c686fd
chriswhsu <c@baci.us>
2023-12-01 20:29:08 -0800

    format csv and stdout consistently.

--- kiln-logger.py
+++ kiln-logger.py
@@ -36,12 +36,11 @@ PID_HEADER = [
     'pid_out',
 ]
 
-
-def logger(hostname, csv_file, no_profile_stats, pid_stats, stdout):
+def logger(hostname, csv_file, profile_stats, pid_stats, stdout):
     status_ws = websocket.WebSocket()
 
     # Determine CSV fields based on flags
-    csv_fields = STD_HEADER if not no_profile_stats else []
+    csv_fields = STD_HEADER if profile_stats else []
     csv_fields += PID_HEADER if pid_stats else []
 
     # Setup CSV writer for file output
@@ -69,28 +68,30 @@ def logger(hostname, csv_file, no_profile_stats, pid_stats, stdout):
             if msg.get('type') == 'backlog':
                 continue
 
-            # Process message for CSV output
-            if not no_profile_stats:
+            # Process message for CSV and stdout output
+            if profile_stats:
                 msg['stamp'] = time.time()
             if pid_stats and 'pid_stats' in msg:
                 msg.update({f"pid_{k}": v for k, v in msg['pid_stats'].items()})
 
-            csv_writer.writerow(msg)
+            formatted_msg = {k: '{:5.3f}'.format(v) if isinstance(v, float) else v for k, v in msg.items()}
+
+            # Write formatted message to CSV file and flush
+            csv_writer.writerow(formatted_msg)
             output_file.flush()
 
-            # Process message for standard output
+            # Write the same formatted message to stdout (if enabled) and flush
             if stdout:
-                csv_stdout.writerow({k: '{:5.3f}'.format(v) if isinstance(v, float) else v for k, v in msg.items()})
+                csv_stdout.writerow(formatted_msg)
                 sys.stdout.flush()
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Log kiln data for analysis.')
     parser.add_argument('--hostname', type=str, default="localhost:8081", help="The kiln-controller hostname:port")
     parser.add_argument('--csv_file', type=str, default="/tmp/kiln_stats.csv", help="Where to write the kiln stats to")
-    parser.add_argument('--pid_stats', action='store_true', help="Include PID stats")
-    parser.add_argument('--no_profile_stats', action='store_true', help="Do not store profile stats (default is to store them)")
-    parser.add_argument('--stdout', action='store_true', help="Also print to stdout")
+    parser.add_argument('--pid_stats', action='store_false', help="Include PID stats")
+    parser.add_argument('--profile_stats', action='store_false', help="store profile stats (default is to store them)")
+    parser.add_argument('--stdout', action='store_false', help="Also print to stdout")
     args = parser.parse_args()
 
-    logger(args.hostname, args.csv_file, args.no_profile_stats, args.pid_stats, args.stdout)
+    logger(args.hostname, args.csv_file, args.profile_stats, args.pid_stats, args.stdout)
