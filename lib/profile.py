0d4d56936d24bd97610cfef1c68473d6110443be
chriswhsu <c@baci.us>
2023-12-13 10:26:37 -0800

    general efficiency updates.

--- lib/profile.py
+++ lib/profile.py
@@ -1,41 +1,39 @@
 import json
+from typing import Tuple, Optional
 
 
 class Profile:
-    def __init__(self, json_data):
+    def __init__(self, json_data: str):
         obj = json.loads(json_data)
         self.name = obj["name"]
-        self.data = sorted(obj["data"])
+        self.data = sorted(obj["data"], key=lambda x: x[0])  # Ensuring sorting by time
 
-    def get_duration(self):
-        return max([t for (t, x) in self.data])
+    def get_duration(self) -> int:
+        """Return the duration of the profile."""
+        return self.data[-1][0] if self.data else 0
 
-    def get_surrounding_points(self, runtime):
-        # If runtime exceeds the total duration, return None for both points
-        if runtime > self.get_duration():
-            return None, None
+    def get_surrounding_points(self, runtime: int) -> Tuple[Optional[Tuple[int, int]], Optional[Tuple[int, int]]]:
+        """Return the data points surrounding the given current_time."""
+        if not self.data or runtime < self.data[0][0]:
+            # If current_time is before the start of the data, return None or the first point
+            return None, self.data[0] if self.data else None
 
-        # Initialize variables for the previous and next points
-        prev_point = None
-        next_point = None
-
-        # Iterate through the data points
-        for i in range(len(self.data)):
-            # Find the point where runtime falls between two data points
+        for i in range(1, len(self.data)):
             if runtime < self.data[i][0]:
-                prev_point = self.data[i - 1]  # The point before the runtime
-                next_point = self.data[i]  # The point after the runtime
-                break  # Stop the loop once the correct points are found
+                return self.data[i - 1], self.data[i]
 
-        # Return the previous and next points
-        return prev_point, next_point
+        return self.data[-2], self.data[-1]
 
-    def get_target_temperature(self, runtime):
-        if runtime > self.get_duration():
+    def get_target_temperature(self, current_time: int) -> float:
+        """Calculate and return the target temperature at the given current_time."""
+        if current_time > self.get_duration():
             return 0
 
-        (prev_point, next_point) = self.get_surrounding_points(runtime)
+        prev_point, next_point = self.get_surrounding_points(current_time)
 
-        incl = float(next_point[1] - prev_point[1]) / float(next_point[0] - prev_point[0])
-        temp = prev_point[1] + (runtime - prev_point[0]) * incl
-        return temp
+        if prev_point is None or next_point is None or prev_point[0] == next_point[0]:
+            return 0
+
+        slope = (float(next_point[1]) - float(prev_point[1])) / (float(next_point[0]) - float(prev_point[0]))
+        temp = prev_point[1] + (current_time - prev_point[0]) * slope
+        return round(temp, 2)
